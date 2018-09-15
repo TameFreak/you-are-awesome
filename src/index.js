b13a58515d1884bed55c82336753525c58d2c245
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-09-15 22:32:13 +0300

    6/10

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
-// DO WHATEVER YOU WANT HERE
+//DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (a) => {return a};
+const createNotEnumerableProperty = (a) => {return Symbol(a)};
+const createProtoMagicObject = () => {return Function};
+
+let counterIncr = 0;
+const incrementor = () => {
+
+    const foo = function () {
+        counterIncr++;
+        return foo;
+    };
+    
+    foo.valueOf = () => counterIncr;
+    return foo();
+}
+
+let counterAsyncIncr = 0;
+const asyncIncrementor = () => {
+
+    const foo = function () {
+        counterAsyncIncr++;
+        return foo;
+    };
+    
+    foo.valueOf = () => counterAsyncIncr;
+    return foo();
+};
+
+class helpAsyncIncrementor{
+    constructor() {
+        this.counter = 0;
+    }
+
+    next() {
+        this.counter++;
+
+        return {
+            done: false,
+            value: this.counter
+        }
+    }
+
+
+    [Symbol.iterator](){
+        return this;
+    }
+}
+
+const createIncrementer = () => {return new helpAsyncIncrementor()};
 
 // return same argument not earlier than in one second, and not later, than in two
 const returnBackInSecond = () => {};
